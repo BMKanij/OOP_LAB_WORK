b2b6b648d1a8e5d7406a6b2f95e127e849a75900
Mst.Kanij Fatima <123645381+BMKanij@users.noreply.github.com>
2023-06-04 23:57:55 +0600

    Delete insuranceinfo.java

--- insuranceinfo.java
+++
@@ -1,9 +0,0 @@
-public class InsuranceInfo {
-    String policyName;
-    int policyId;
-    public InsuranceInfo(String policyName, int policyId)
-    {
-        this.policyId=policyId;
-        this.policyName=policyName;
-    }
-}
